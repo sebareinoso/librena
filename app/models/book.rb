3de07cbf72b9cd84cba7aaed35718f398f6ae1d0
Sebastian Reinoso <sebastian.reinoso@proton.me>
2025-10-15 18:03:20 -0300

    Refactor book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,21 +3,4 @@
 class Book < ApplicationRecord
   validates :title, presence: true, length: { minimum: 2 }
   validates :summary, presence: true
-  validate :image_validation
-
-  has_one_attached :image
-
-  private
-
-  def image_validation
-    return unless image.attached?
-
-    unless image.content_type.in? %w[image/png image/jpg image/jpeg image/webp]
-      errors.add :image, 'must be a PNG, JPG, or WEBP'
-    end
-
-    return unless image.byte_size > 1.megabyte
-
-    errors.add :image, 'is too large (max is 1 MB)'
-  end
 end
