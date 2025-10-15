64281717af65de520e87c7e683ebb9e3916329e4
Sebastian Reinoso <sebastian.reinoso@proton.me>
2025-10-15 15:08:34 -0300

    Refactor test to be more syntactically concise

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -4,30 +4,24 @@ require 'rails_helper'
 
 RSpec.describe Book, type: :model do
   describe 'validations' do
-    it 'is not valid without title' do
-      book = described_class.new(summary: 'Book A summary')
-
-      aggregate_failures do
+    context 'when title is missing' do
+      it 'is invalid' do
+        book = described_class.new(summary: 'Book A summary')
         expect(book).not_to be_valid
-        expect(book.errors[:title]).to include("can't be blank")
       end
     end
 
-    it 'is not valid without summary' do
-      book = described_class.new(title: 'Book A')
-
-      aggregate_failures do
+    context 'when summary is missing' do
+      it 'is invalid' do
+        book = described_class.new(title: 'Book A')
         expect(book).not_to be_valid
-        expect(book.errors[:summary]).to include("can't be blank")
       end
     end
 
-    it 'is not valid with a title shorter than 2 characters' do
-      book = described_class.new(title: 'A', summary: 'Book A summary')
-
-      aggregate_failures do
+    context 'when title is too short (minimum is 2 characters)' do
+      it 'is invalid' do
+        book = described_class.new(summary: 'A')
         expect(book).not_to be_valid
-        expect(book.errors[:title]).to include('is too short (minimum is 2 characters)')
       end
     end
   end
