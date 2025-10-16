8ef0a36cfe34845d71eef3ed31560f28e5a1ba2d
Sebastian Reinoso <sebastian.reinoso@proton.me>
2025-10-15 21:32:35 -0300

    Add User tests

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'validations' do
+    context 'when username is missing' do
+      it 'is not invalid' do
+        user = described_class.new email: 'test@test.com', password: '123456'
+        expect(user).not_to be_valid
+      end
+    end
+
+    context 'when username is not unique' do
+      it 'is not invalid' do
+        described_class.create! username: 'Mr rater', email: 'test@test.com', password: '123456'
+        user = described_class.new username: 'Mr rater', email: 'test2@test.com', password: '123456'
+        expect(user).not_to be_valid
+      end
+    end
+
+    context 'when email is missing' do
+      it 'is not invalid' do
+        user = described_class.new username: 'Mr rater', password: '123456'
+        expect(user).not_to be_valid
+      end
+    end
+
+    context 'when email is not unique' do
+      it 'is not invalid' do
+        described_class.create! username: 'Mr rater', email: 'test@test.com', password: '123456'
+        user = described_class.new username: 'Mrs rater', email: 'test@test.com', password: '123456'
+        expect(user).not_to be_valid
+      end
+    end
+
+    context 'when password is missing' do
+      it 'is not invalid' do
+        user = described_class.new username: 'Mr rater', email: 'test@test.com', password_confirmation: '123456'
+        expect(user).not_to be_valid
+      end
+    end
+
+    context 'when password is too short (min 6 characters)' do
+      it 'is not invalid' do
+        user = described_class.new username: 'Mr rater', email: 'test@test.com', password: '12345'
+        expect(user).not_to be_valid
+      end
+    end
+  end
+end
